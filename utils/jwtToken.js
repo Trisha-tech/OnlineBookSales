df0a0f3b1adcefb905e61fb71f8156c9af991ef5
Subhash <Subhash613d@gmail.com>
2024-05-10 22:20:18 +0530

    added cart routes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,34 +1,34 @@
 const jwt = require('jsonwebtoken');
-const sendToken = (user, statusCode, res) => {
-  // Create JWT token
-  const token = (user) => jwt.sign(user, process.env.COOKIE_EXPIRE, { expiresIn: process.env.JWT_EXPIRE });
-  res.status(statusCode).cookie("token", token).json({
-    success: true,
-    user,
-    token,
-  });
-};
-
-
-// Create Token and saving in cookie
-
 // const sendToken = (user, statusCode, res) => {
-//     // Create JWT token
+//   // Create JWT token
+//   const token = (user) => jwt.sign(user, process.env.COOKIE_EXPIRE, { expiresIn: process.env.JWT_EXPIRE });
+//   res.status(statusCode).cookie("token", token).json({
+//     success: true,
+//     user,
+//     token,
+//   });
+// };
 
-//     const token = user.getJWTToken();
 
-//     // options for cookie
-//     const options = {
-//       expires: new Date(
-//         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-//       ),
+//Create Token and saving in cookie
+
+const sendToken = (user, statusCode, res) => {
+    // Create JWT token
+
+    const token = user.getJWTToken();
+
+    // options for cookie
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
       
-//       httpOnly: true,
-//     };
-//     res.status(statusCode).cookie("token", token).json({
-//       success: true,
-//       user,
-//       token,
-//     });
-//   };
+      httpOnly: true,
+    };
+    res.status(statusCode).cookie("token", token).json({
+      success: true,
+      user,
+      token,
+    });
+  };
   module.exports = sendToken;
