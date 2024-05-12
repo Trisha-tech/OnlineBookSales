c812d84d3ef4eb036e9b6d0489b114e7c922195a
sahithi ghanta <99321888+sahi123-cloud@users.noreply.github.com>
2024-05-12 20:39:11 +0530

    adjusted signup

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/LoginPage";
 import SignUpPage from "./Components/SignUpPage";
-
+import Product from "./Components/Product";
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +13,7 @@ function App() {
         {/* Adjusted route path */}
         <Route path="/login" element={<LoginPage />} />{" "}
         {/* Adjusted route path */}
+        <Route path="/product" element={<Product/>}/>
       </Routes>
     </BrowserRouter>
   );
