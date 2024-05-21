e01c915713ecc37bbd0f7eac45c4760e3e667327
ASHISH-JHA94 <ajha94023@gmail.com>
2024-05-21 19:00:12 +0530

    improve UI of NavBar and correct the file structure add the context

---
+++ client/src/Pages/Cart.jsx
@@ -0,0 +1,9 @@
+function Cart(){
+    return(
+        <div>
+            this will contain information related to carts.
+        </div>
+    )
+}
+
+export default Cart;
---
+++ client/src/Pages/Orders.jsx
@@ -0,0 +1,9 @@
+function Orders(){
+    return(
+        <div>
+            this will contain information related to Orders.
+        </div>
+    )
+}
+
+export default Orders;
