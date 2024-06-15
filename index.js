ad2fadff77fff0569231283be06a54ec8adf3fd7
Sneha Menat <112338459+Sneha0019@users.noreply.github.com>
2024-06-15 17:00:40 +0530

    final Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require(`express`);
-const path=require('path');
+const path = require('path');
 const app = express();
 const dotenv = require(`dotenv`);
 const mongoose = require("mongoose");
@@ -64,4 +64,8 @@ app.use("admin", authorizeRoles, admin);
 app.use(errorMiddleware);
 app.get("/", (req, res) => {
   res.send(`Welcome to Scizers Assignment !!!    Made by Trisha Sahu`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
