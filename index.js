7a9b40b79b7f3d70abe95ae291951af95a2b1bcd
Sneha Menat <snehamenat029@gmail.com>
2024-06-15 18:44:13 +0530

    final commit

--- index.js
+++ index.js
@@ -64,4 +64,8 @@ app.use("admin", authorizeRoles, admin);
 app.use(errorMiddleware);
 app.get("/", (req, res) => {
   res.send(`Welcome to Scizers Assignment !!!    Made by Trisha Sahu`);
-});
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
