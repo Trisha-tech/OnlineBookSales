9f60451ca14d4184e607be2844534c3662e225b8
Sneha Menat <112338459+Sneha0019@users.noreply.github.com>
2024-06-15 16:29:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,30 +33,30 @@ if (!MONGO_URL) {
 
 // MongoDB connection
 mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB");
-  });
-  mongoose.connection.on("error", (err) => {
-    console.log("Error Connecting to Database", err);
-  });
-  /*MONGODB CONNECTION END*/
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error Connecting to Database", err);
+});
+/*MONGODB CONNECTION END*/
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route Imports
-const customerRoutes = require('./routes/customerRoutes.js');
-const productRoutes = require('./routes/productRoutes.js');  // Make sure this import statement is correct
-const orderRoutes = require('./routes/orderRoutes.js');
-const adminRoutes = require('./routes/adminRoutes.js');
-const { authorizeRoles } = require('./middlewares/auth.js');
+const customer = require("./routes/customerRoutes.js");
+const product = require("./routes/productRoutes.js");
+const order = require("./routes/orderRoutes.js");
+const admin = require("./routes/adminRoutes.js");
+const { authorizeRoles } = require("./middlewares/auth.js");
 
 app.use("/customer", customer);
-app.use("/api/product", productRoutes);
+app.use("/product", product);
 app.use("/order", order);
 
 app.use("admin", authorizeRoles, admin);
