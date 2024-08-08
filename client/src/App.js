28ae7dd61a61a946a3ae68829066558dd48eb975
Mastan Sayyad <sayyadmastan78@gmail.com>
2024-08-08 16:16:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Shop from "./Pages/Shop.jsx";
 import { Toast } from "./Toast/Toast.js";
 import Contact from "./Pages/Contact.jsx";
 import PrivacyPolicy from "./Pages/PrivacyPolicy.jsx";
+import TermsAndConditions from "./Pages/Terms.jsx";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './Animations.css';
 import FAQ from "./Pages/Faq.jsx";
@@ -59,6 +60,7 @@ function App() {
             <Route path="/about" element={<AboutUs/>}/>
             <Route path="/faqs" element={<FAQ/>}/>
             <Route path="/privacy" element={<PrivacyPolicy />}/>
+            <Route path="/terms" element={<TermsAndConditions />}/>
           </Routes>
           <Toast position="bottom-right" />
           <Footer />
