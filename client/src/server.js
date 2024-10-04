0e14f29d93070c4910070aae96db4ad70e5d3af1
Pushkar Dev <pushkardev54321@gmail.com>
2024-10-04 19:05:39 +0530

    Reviewed user fetching / creating new user

--- client/src/server.js
+++ client/src/server.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const User = require('./models/userSchema'); // Import User model
 
 const app = express();
 const port = 5000;
@@ -23,25 +26,56 @@ app.get('/api/orders', (req, res) => {
     res.json(orders);
 });
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
 // Passport configuration for Google OAuth
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID, 
+    clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5000/auth/google/callback"
 },
-function (accessToken, refreshToken, profile, done) {
-    // Here you can check if the user exists in your DB and create one if not
-    // For simplicity, we'll just return the profile
-    return done(null, profile);
+async (accessToken, refreshToken, profile, done) => {
+    try {
+        // Check if the user exists in the database
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (user) {
+            // If user exists, return the user
+            return done(null, user);
+        } else {
+            // If user doesn't exist, create a new user
+            user = new User({
+                googleId: profile.id,
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                avatar: profile.photos[0].value
+            });
+            await user.save(); // Save the new user in the database
+            return done(null, user);
+        }
+    } catch (err) {
+        console.error(err);
+        return done(err, null);
+    }
 }));
 
 // Serialize and deserialize user for session management
 passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, user.id);
 });
 
-passport.deserializeUser((user, done) => {
-    done(null, user);
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
 });
 
 // Setup session
