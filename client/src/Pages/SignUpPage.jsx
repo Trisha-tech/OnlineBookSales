9ed1a376715fbdd1e640bdd43aa4960426555237
Anneshu Nag <132702983+NK-Works@users.noreply.github.com>
2024-10-04 15:02:25 +0530

    signup-ui-updated

--- client/src/Pages/SignUpPage.jsx
+++ client/src/Pages/SignUpPage.jsx
@@ -27,7 +27,7 @@ const SignUpPage = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post("http://localhost:8080/customer/register", { name, email, password})
+            const response = await axios.post("http://localhost:8080/customer/register", { name, email, password })
 
             console.log(response.data);
             toast.success("register sucess");
@@ -52,66 +52,70 @@ const SignUpPage = () => {
 
     return (
 
-        <><Preloader/>
-        <Container maxWidth="xl">
-            <Toaster/>
-            <div style={{ marginTop: "100px", marginBottom: "180px" }}>
+        <><Preloader />
+            <Container maxWidth="xl">
+                <Toaster />
+                <div style={{ marginTop: "50px", marginBottom: "50px" }}>
 
-                <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-                    <Grid item xs={12} md={6}>
-                        <Box sx={{ display: { xs: "none", md: "block" } }}>
-                            <Lottie animationData={loginAnimation} style={{ height: '500px' }} />
-                        </Box>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        md={6}
-                        sx={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                    >
-
-                        <form onSubmit={handleSubmit}>
-                            <Typography variant="h5" align="center" gutterBottom className="dark:text-white">
-                                Register
-                            </Typography>
-                            <input
-                                placeholder="Name"
-                                variant="outlined"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                margin="normal"
-                                className="w-full mb-4 p-3 bg-transparent border border-black rounded-md dark:text-white"
-                            />
-                            <input 
-                                placeholder="Email"
-                                variant="outlined"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                margin="normal"
-                                className="w-full mb-4 p-3 bg-transparent border border-black rounded-md dark:text-white"
-                            />
-                            <Box sx={{ position: "relative", display: "flex", alignItems: "center" }}>
-                                <input 
-                                    placeholder="Password"
-                                    variant="outlined"
-                                    value={password}
-                                    type={showPassword ? "text" : "password"} // Set input type dynamically
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className="w-full p-3 bg-transparent border border-black rounded-md dark:text-white"
-                                    margin="normal"
-                                />
-                                <IconButton
-                                    onClick={togglePasswordVisibility}
-                                    sx={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)" }}
-                                    >
-                                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                                </IconButton>
+                    <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+                        <Grid item xs={12} md={6}>
+                            <Box sx={{ display: { xs: "none", md: "block" } }}>
+                                <Lottie animationData={loginAnimation} style={{ height: '500px' }} />
                             </Box>
-                            {/* <TextField
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            md={6}
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+
+                            <form onSubmit={handleSubmit} className="w-full max-w-[400px] flex flex-col bg-white dark:bg-[#1e1e1e] p-8 rounded-xl shadow-2xl transform transition-all duration-300 hover:scale-105">
+                                <Typography variant="h5" align="center" gutterBottom className="dark:text-white">
+                                    <h1 className='text-2xl md:text-3xl text-[#060606] font-semibold mb-1 mt-1 dark:text-white'>Book4U</h1>
+                                    <h2 className='text-lg md:text-xl mb-1'>Signup</h2>
+                                </Typography>
+                                <input
+                                    placeholder="Name"
+                                    variant="outlined"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    margin="normal"
+                                    className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none dark:text-white dark:border-white"
+                                />
+                                <input
+                                    placeholder="Email"
+                                    variant="outlined"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    margin="normal"
+                                    className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none dark:text-white dark:border-white"
+                                />
+                                <Box sx={{ position: "relative", display: "flex", alignItems: "center" }}>
+                                    <input
+                                        placeholder="Password"
+                                        variant="outlined"
+                                        value={password}
+                                        type={showPassword ? "text" : "password"} // Set input type dynamically
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none dark:text-white dark:border-white"
+                                    />
+                                    <IconButton
+                                        onClick={togglePasswordVisibility}
+                                        sx={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)" }}
+                                    >
+                                        {showPassword ? (
+                                            <VisibilityIcon className="text-black dark:text-white" /> // Text color changes based on theme
+                                        ) : (
+                                            <VisibilityOffIcon className="text-black dark:text-white" />
+                                        )}
+                                    </IconButton>
+                                </Box>
+                                {/* <TextField
                                 label="Phone"
                                 fullWidth
                                 variant="outlined"
@@ -127,23 +131,36 @@ const SignUpPage = () => {
                                 onChange={(e) => setAddress(e.target.value)}
                                 margin="normal"
                             /> */}
-                            {error && <Typography color="error" align="center">{error}</Typography>}
-                            <Button
-                                variant="contained"
-                                type="submit"
-                                fullWidth
-                                sx={{
-                                    mt: 2,
-                                    "&:hover": { backgroundColor: "#0069d9" },
-                                }}
-                            >
-                                Register
-                            </Button>
-                        </form>
+                                {error && <Typography color="error" align="center">{error}</Typography>}
+                                <button
+                                    variant="contained"
+                                    type="submit"
+                                    fullWidth
+                                    sx={{
+                                        mt: 2,
+                                        "&:hover": { backgroundColor: "#0069d9" },
+                                    }}
+                                    className="w-full text-white my-2 mt-5 font-semibold bg-[#060606] rounded-md p-4 text-center flex items-center justify-center dark:bg-white dark:text-black"
+                                >
+                                    Register
+                                </button>
+                                <div className='w-full flex items-center justify-center mt-3 mb-3'>
+                                    <p className='text-sm text-[#060606] dark:text-white'>
+                                        Already have an account?{' '}
+                                    </p>
+                                </div>
+                                <button
+                                    type="button"
+                                    className='w-full text-white my-2 font-semibold bg-[#060606] border border-black/40 rounded-md p-4 text-center flex items-center justify-center dark:bg-white dark:text-black'
+                                    onClick={() => navigate('/login')}
+                                >
+                                    Login
+                                </button>
+                            </form>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </div>
-        </Container>
+                </div>
+            </Container>
         </>
     );
 };
