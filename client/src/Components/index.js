1e9a6de4df09209afe55c0e16556143017096252
Rohit yadav <151960291+rohir1132yadav@users.noreply.github.com>
2024-10-11 20:06:23 +0530

    add box shadow and hover effect
    i have add box shadow and hover effect in different html file and add curser pointer.

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,11 +1,15 @@
-
-import Profile from "./CustomerProfile/Profile.jsx";
+import Header from "./Header/Header.js";
+import ProfilePage from "./Profile/ProfilePage";
+import HomePage from "./HomePage";
 import Product from "./Product.jsx"
 import Footer from "./Footer/Footer.jsx"
-import Navbar from "./NavBar/NavBar.jsx"
+import Navbar from "./NavBar/NavBar.jsx";
+
 export {
-    Profile,
+    Header,
+    ProfilePage,
+    HomePage,
     Product,
     Footer,
-    Navbar
+    Navbar,
 }
