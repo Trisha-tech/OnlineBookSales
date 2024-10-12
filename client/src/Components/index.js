70fc7ed17725b9c13a08931b0a226a75f00a1022
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-10-12 21:56:21 +0530

    resolved to top overlap issue

---
+++ client/src/Components/index.js
@@ -0,0 +1,10 @@
+import Profile from "./CustomerProfile/Profile.jsx";
+import Product from "./Product.jsx"
+import Footer from "./Footer/Footer.jsx"
+import Navbar from "./NavBar/NavBar.jsx"
+export {
+    Profile,
+    Product,
+    Footer,
+    Navbar
+}
