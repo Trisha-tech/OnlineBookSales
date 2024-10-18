dad4a8ce68cc74536d178ebd75d90e0c4bc6bc89
Dinky Rajpoot <111672121+DinkyRajpoot56@users.noreply.github.com>
2024-10-18 20:19:50 +0530

    Added About.jsx

---
+++ client/src/About.jsx
@@ -0,0 +1,15 @@
+<div className='w-full h-[90vh]'>
+      <img src='https://hips.hearstapps.com/hmg-prod/images/hbz-travel-index4-1-1547062721.gif?crop=1.00xw:1.00xh;0,0&resize=1200:*'
+      className='w-full h-full object-cover'
+      />
+      <div className='max-w-[1140px] m-auto'>
+      <div className='absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-white p-4'>
+        <h1 className='font-bold text-4xl'>Explore Beauty of The World</h1>
+        <h2 className='text-4xl py-4 italic'>With WEEKAWAY</h2>
+        <p> 
+        Exploring new books unveils the richness of human diversity and the vastness of natural wonders, 
+        providing unforgettable experiences and lifelong memories.A book is a medium for recording information in the form of writing or images. Modern books are typically in codex format, composed of many pages that are bound together and protected by a cover; they were preceded by several earlier formats, including the scroll and the tablet.
+        </p>
+      </div>
+      </div>
+    </div>
